2b3a1a911fe17483546a73970175946f5d60bfae
Tamim Dostyar <a.tamimdostyar@gmail.com>
2026-04-05 22:51:04 -0500

    wrote first program

---
+++ app.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    double gpa;
+    std::string name;
+
+    std::cout << "what is your name? ";
+    std::cin >> name;
+
+    std::cout << "what is your gpa? ";
+    std::cin >> gpa;
+
+    // now get those and print them
+    std::cout << "Name: " << name << " GPA: " << gpa << std::endl;
+    return 0;
+}
