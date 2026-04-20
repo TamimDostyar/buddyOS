12cd6b4ab621a299ec51cc6823342d450b9e53d9
Tamim Dostyar <a.tamimdostyar@gmail.com>
2026-04-20 00:09:03 -0500

    fixed

--- kernel/src/manager/kmain.c
+++ kernel/src/manager/kmain.c
@@ -1,31 +1,30 @@
-#include "../utils/vga.h"
-#include "../utils/keyboard.h"
 #include "../utils/idt.h"
+#include "../utils/keyboard.h"
+#include "../utils/vga.h"
 #include "heap.h"
 
 void kmain(void) {
 
-    int vgaOutput = vga_init();
-    int keyboardOutput = keyboard_init();
-    int idtOutput = idt_install();
+  int vgaOutput = vga_init();
+  int keyboardOutput = keyboard_init();
+  int idtOutput = idt_install();
 
-    // heap
-    heap_init();
-    int *memory = kmalloc(sizeof(memory));
-    kfree(memory);
-    
-    // set interrupt 
-    __asm__ volatile("sti");
-    vga_write("Kernell is working and running successfully\n");
-    vga_write("\n");
-    vga_write("buddyOS> ");
-    vga_lock_cursor();
+  // heap
+  heap_init();
+  int *memory = kmalloc(sizeof(memory));
+  kfree(memory);
 
+  // set interrupt
+  __asm__ volatile("sti");
+  vga_write("Kernell is working and running successfully\n");
+  vga_write("\n");
+  vga_write("buddyOS> ");
+  vga_lock_cursor();
 
-    for (;;) {
-        char c = keyboard_getchar();
-        if (c != 0) {
-            vga_putchar(c);
-        }
+  for (;;) {
+    char c = keyboard_getchar();
+    if (c != 0) {
+      vga_putchar(c);
     }
+  }
 }
