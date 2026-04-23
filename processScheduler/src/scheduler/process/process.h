e09620e992723ee3c3f3f421393c722f50bd0f24
Tamim Dostyar <a.tamimdostyar@gmail.com>
2026-04-23 12:17:50 -0500

    process dynamically allocate memory

--- processScheduler/src/scheduler/process/process.h
+++ processScheduler/src/scheduler/process/process.h
@@ -1,9 +1,12 @@
 #include "heap.h"
 
+
+
+
 typedef struct {
     // int processState; // 1 for running, 0 for terminate, 2 waiting and 3 read
     // int processState[MAX_PRCESS]
-    int *processState = kmalloc(sizeof(memory)); // dynamically allocate to avoid memory limit
+    int *processTable; // dynamically allocate to avoid memory limit
     int processNumber; // bascially the pid for each process
     
 } Pid_t;
