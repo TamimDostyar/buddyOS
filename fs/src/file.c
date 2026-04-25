1aa235a6bf276be83fb1904cfb92d68940293f12
Tamim Dostyar <a.tamimdostyar@gmail.com>
2026-04-25 17:19:53 -0500

    complete

--- fs/src/file.c
+++ fs/src/file.c
@@ -1,13 +1,10 @@
 #include "debug.h"
+#include "file.h"
+
 
 int main(void) {
 
-    char buff[120] = "This is a test to see if print works";
-    int number = 1;
-    if (number == 1){
-        kprintf_str("hello this is just a number\n");
-        kprintf_str(buff);
-        kprintf_int(number);
-    }
+
+
     return 0;
 }
