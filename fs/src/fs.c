0e11f1c43b39d587433015fa12677234d11e5ac6
Tamim Dostyar <a.tamimdostyar@gmail.com>
2026-05-02 23:55:14 -0500

    permission

---
+++ fs/src/fs.c
@@ -0,0 +1,8 @@
+#include "debug.h"
+#include "fs.h"
+
+
+void mount(void){
+    kprintf_str("Mounting system\n");
+
+}
---
+++ fs/src/fs.h
@@ -0,0 +1,18 @@
+#ifndef FILESYSTEMS
+#define FILESYSTEMS
+
+enum {
+    paretnDir = '\0';
+    mountSystem = '\0';
+    systemBlock = '\0';
+}
+
+
+typedef struct{
+    space;
+    
+} SystemsMounting;
+void mount(void);
+
+#endif
+
