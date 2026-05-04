afc82f2f5bfe5b80c2c79c585738d57ab247c740
Tamim Dostyar <a.tamimdostyar@gmail.com>
2026-05-03 21:27:40 -0500

    remodeling

--- fs/src/fs.h
+++ fs/src/fs.h
@@ -1,18 +1,25 @@
 #ifndef FILESYSTEMS
 #define FILESYSTEMS
 
-enum {
-    paretnDir = '\0';
-    mountSystem = '\0';
-    systemBlock = '\0';
-}
+#define MAX_FILE_ENTRY 32
+typedef struct FileEntry_t{
+    char f_name[MAX_FILE_ENTRY];
+    uint32_t size;
+    uint32_t start_block;
+} FileEntry_t;
 
+typedef struct fileSystemState_t{
+    superblock super;
+    FileEntry_t* directory;
 
-typedef struct{
-    space;
-    
-} SystemsMounting;
-void mount(void);
+}fileSystemState_t;
+
+// function prototypes
+void fs_mount(void);
+int fs_create(const char* name, uint32_t size, const void* data);
+int fs_read(const char* name, void* buffer);
+int fs_delete(const char* name);
+void fs_list(void);
 
 #endif
 
--- fs/src/superblock.h
+++ fs/src/superblock.h
@@ -1,38 +1,16 @@
-#include <stdlib.h>
-
-#ifndef SUPERBLOCKS
-#define SUPERBLOCKS
+#ifndef SIMPLE_SUPERBLOCK_H
+#define SIMPLE_SUPERBLOCK_H
 
 
-// this is basically the table of content
+typedef struct superblock {
+    uint32_t magic;         
+    uint32_t block_size;    
+    uint32_t total_blocks;  
 
-typedef struct __super_block_t{
-    // identity  and total number for partition
-    uint32_t magic;
+    uint32_t dir_start_block;
+    uint32_t dir_entry_count; 
 
-    uint32_t sector_count;
-    uint32_t inode_count;
+    uint32_t data_start_block;
+} superblock_t;
 
-    // total size from partition
-    uint32_t partition_lba_base;
-    uint32_t block_bitmap_lba;
-    uint32_t block_bitmap_sectors;
-
-    // bitmap location
-
-    // lba is the logical block address
-    // and inode is the datastructure where it stores things like read/write modification date....
-    uint32_t inode_bitmap_lba;
-    uint32_t inode_bitmap_sectors;
-    uint32_t inode_table_lba;
-    uint32_t inode_table_sects;
-
-    uint32_t data_start_lba;
-    uint32_t root_inode_no;
-    uint32_t dir_entry_size;
-
-
-} __attribute__((packed)) super_block_t;
-
-// the logic is inspired from JackOS
 #endif
